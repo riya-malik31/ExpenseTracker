57d6c55628ef3f17c6f1a1fe0f08bb26c1dca7f0
riya-malik31 <riyacws123@gmail.com>
2025-04-07 17:45:59 +0530

    Update Signup.jsx

--- expense-tracker-app-new/src/Components/Signup.jsx
+++ expense-tracker-app-new/src/Components/Signup.jsx
@@ -13,7 +13,7 @@ function Signup() {
   event.preventDefault();
 
   try {
-    const response = await axios.post("http://localhost:5000/signup", inputs);
+    const response = await axios.post("http://localhost:5000/signup","https://expensetracker-2-edh9.onrender.com", inputs);
     alert(response.data.message); 
     navigate("/Login")
   } catch (error) {
